3396f5783f486b87e88ee3721383db01fd5f3b43
Chinonso <chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.com>
2022-09-26 12:31:25 +0100

    My Javascript project

---
+++ myFirst.js
@@ -0,0 +1,35 @@
+document.getElementById("demo").innerHTML = "This is agba coder class";
+document.getElementById("memo").innerHTML = "This suppose to be group two";
+document.getElementById("life").innerHTML = "Life is fucking hard, regardless of what you do";
+document.getElementById("human").innerHTML = "Human can be tamed when you reciprocate the level of energy given";
+document.getElementById("pet").innerHTML = "Pets are so uneccessary to human existence. They are waste a of space";
+document.getElementById("money").innerHTML = "Money is a neccessity but patience is required in it's pursuit";
+document.getElementById("when").innerHTML = "I reaffirm my position on the need for wisdom";
+
+document.getElementById("demo").style.fontSize = "60px";
+document.getElementById("memo").style.fontSize = "45px";
+document.getElementById("life").style.fontSize = "53px";
+
+document.write("5977 + 9988");
+
+console.log('this a whole lot of teaching');
+console.log(2000 + 9908);
+
+
+let x = 90;
+let y = 100;
+let z = x * y;
+
+console.log(x*y);
+
+const group1 = ['Chima', 'Luke', 'Chinonso', 'Ruth', 'Sandra'];
+
+const ul = document.querySelector('ul')
+
+for (const member of group1) {
+  const li = document.createElement('li');
+  li.innerHTML = member;
+  ul.appendChild(li);
+
+  li.innerHTML = member;
+}
---
+++ playGround.js
@@ -0,0 +1,64 @@
+// console.log('Chinonso');
+// console.log('Enugu is  coal city');
+
+// name = 'Scholes Dammit'
+// console.log(name);
+
+// sentence = 'How are you? You good? How can i be of service to you'
+// console.log(sentence);
+
+// food = Number(prompt('How much was your food?'))
+// tipPercentage = Number(prompt('Tip %'))/100
+// tipAmount = food * tipPercentage
+// Total = food + tipAmount
+
+// console.log('tip amount:', tipAmount);
+// console.log('total:', Total);
+
+
+// alert(tipAmount)
+// fruit = prompt('what is your favorite fruit?')
+// console.log(fruit);
+
+Math.floor(234.33)
+// console.log(Math.floor);
+
+// let weather = prompt('How is the weather?')
+
+// if (weather == 'rain') {
+//   console.log('Grab your umbrella');
+// } else {
+//   console.log('Wear your sun glasses');
+// }
+
+function sayMyName(){
+  console.log('Namisu');
+ 
+}
+
+// sayMyName()
+
+
+function sayMyName2(name){
+  console.log(name);
+ 
+}
+
+// sayMyName2('Bilkisu')
+
+function greeting(name) {
+  console.log( `Hello ${name}! How are you doing today?`)
+}
+ 
+greeting('Suzan')
+
+function sum(a, b) {
+  return a + b
+}
+
+
+
+console.log(sum(6, 9))
+
+
+ 
